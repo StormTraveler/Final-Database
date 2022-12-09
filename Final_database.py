c306c0013c438def7f4bb898ec15366c947571fa
StormTraveler <95397529+StormTraveler@users.noreply.github.com>
2022-12-09 16:34:39 -0600

    Update Final_database.py

--- Final_database.py
+++ Final_database.py
@@ -1,1 +1,28 @@
 import sqlite3
+
+
+def __main__():
+    conn = sqlite3.connect("Final_database.db")
+    cur = conn.cursor()
+    table = (''' Create Table Final_database (Student_ID INTEGER PRIMARY KEY NOT NULL,
+                                                 FirstName TEXT NOT NULL,
+                                                 LastName TEXT,
+                                                 Gender TEXT,
+                                                 GraduationYear INT,
+                                                 Major TEXT,
+                                                 EmploymentStatus TEXT ''')
+
+    cur.execute(''' INSERT INTO Final_Database (
+                                       Student_ID,
+                                       FirstName,
+                                       LastName,
+                                       Gender,
+                                       GraduationYear,
+                                       Major,
+                                       EmploymentStatus)
+                                       VALUES (1456, 'Bob', 'Johnson', 'Male', 2028, 'Computer Science', 'Full Time')''')
+
+    print(table)
+    conn.commit()
+    conn.close()
+
